939e69b062cd7d0765053954116ab5511844f659
Brandon Duncan <duncanb@spu.edu>
2018-07-08 11:19:28 -0700

    Added google map to home screen. I'm glad that's over. Also cleaned up some other things.

--- EZShopper/EZShopper.Android/MainActivity.cs
+++ EZShopper/EZShopper.Android/MainActivity.cs
@@ -2,14 +2,17 @@
 
 using Android.App;
 using Android.Content.PM;
-using Android.Views;
 using Android.OS;
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+
 
 namespace EZShopper.Droid
 {
     [Activity(Label = "EZShopper", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+       
         protected override void OnCreate(Bundle bundle)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
@@ -17,9 +20,12 @@ namespace EZShopper.Droid
 
             base.OnCreate(bundle);
 
-            global::Xamarin.Forms.Forms.Init(this, bundle);
+            Forms.Init(this, bundle);
+            Xamarin.FormsMaps.Init(this, bundle);
+            
             LoadApplication(new App());
         }
+
     }
 }
 
